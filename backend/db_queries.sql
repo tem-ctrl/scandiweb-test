68caef10d5b5d942ed657bf2adfc2c073113aec6
Gilbert Temgoua <gilbertemgoua@gmail.com>
2023-04-10 12:32:02 +0100

    build agail

--- backend/db_queries.sql
+++ backend/db_queries.sql
@@ -27,28 +27,16 @@ CREATE TABLE IF NOT EXISTS id20543364_scandiweb_test_db.furniture(
 );
 -- Insert mock data to database
 INSERT INTO id20543364_scandiweb_test_db.book
-VALUES ("GGWES5E7D", "CORS in action", 49.99, 0.42),
-  ("GGW0RFZ85", "React in action", 44.99, 0.62),
-  ("GGW865R8F", "PHP & MySQL", 49.99, 1.52);
+VALUES ("GGWES5E7D","CORS in action",49.99,0.42),
+  ("GGW0RFZ85","React in action",44.99,0.62),
+  ("GGW865R8F","PHP & MySQL",49.99,1.52);
+
 INSERT INTO id20543364_scandiweb_test_db.dvd
 VALUES ("JVCES5E7D", "Debesskrāpis", 28.99, 6200),
-  ("JVC0RFZ85", "The Mover", 22.8, 5000),
-  ("JVC865R8F", "Viļņu Varā", 8.8, 2048);
+  ("JVC0RFZ85","The Mover",22.8,5000),
+  ("JVC865R8F","Viļņu Varā",8.8,2048);
+
 INSERT INTO id20543364_scandiweb_test_db.furniture
-VALUES ("TR1ES5E7D", "Sideboard", 255, 48, 90, 140),
-  (
-    "TR10RFZ85",
-    "Mobile Pedestal File",
-    110,
-    50,
-    50,
-    90
-  ),
-  (
-    "TR1865R8F",
-    "Twin Bed Mattress",
-    210,
-    26,
-    160,
-    200
-  );
+VALUES ("TR1ES5E7D","Sideboard",255,48,90,140),
+  ("TR10RFZ85","Mobile Pedestal File",110,50,50,90),
+  ("TR1865R8F","Twin Bed Mattress",210,26,160,200);
